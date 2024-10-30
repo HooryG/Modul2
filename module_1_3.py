bce58a081caf7a66ecc4f3cd71714ddf9ea22b81
HooryG <hooryegor@gmail.com>
2024-10-30 16:52:28 +0900

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+from operator import truediv
+
+name = ("Konstantin")
+print(name,type(name))
+
+age = (48)
+print(age,type(age))
+age = (48+2)
+print(age,type(age))
+name =("i_student")
+print(name,type(name),bool(name))
