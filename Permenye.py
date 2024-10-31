17bec34d05e13b023565c25f79cfd9d234cb3dfe
HooryG <hooryegor@gmail.com>
2024-10-31 18:02:11 +0900

    M1task2

--- Permenye.py
+++ Permenye.py
@@ -1,8 +1,21 @@
-made_home_task = 12
-print (made_home_task)
-hours_spent = 1.5
-print(hours_spent)
-course_name = "python"
-print (hours_spent /made_home_task)
-time_one_task=(float(hours_spent/made_home_task))
-print(made_home_task,hours_spent,course_name,time_one_task)
+course_name = "Курс: Python"
+print (course_name)
+made_home_task = int(12)
+print(made_home_task)
+name_made_home_task= "Колличество заданий"
+print (name_made_home_task)
+print(name_made_home_task,made_home_task)
+hours_spent =float(1.5)
+name_hours_spent = "Количество потраченых часов"
+print(name_hours_spent)
+print(name_hours_spent,hours_spent)
+time_one_task = (hours_spent/made_home_task)
+print(float(time_one_task))
+name_time_one_task="Потраченное время на одно задание"
+print(time_one_task,name_time_one_task)
+print(course_name,"/",name_made_home_task,made_home_task,"/",name_hours_spent,hours_spent,"/",name_time_one_task,time_one_task)
+#Еслиб я знал как обращаться к строке напряму сделал бы короче. По заданию я понял, что выводить нужно с текстом.
+#Могу убрать проверочные print.
+#print (float hours_spent)/int made_home_task)
+#time_one_task=(float (0.125)),(str("Время на одно задание"))
+#print(made_home_task,hours_spent,course_name,time_one_task)
