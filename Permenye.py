4c66075022c8067830380bad9a728d3587233c4a
HooryG <hooryegor@gmail.com>
2024-10-31 16:57:08 +0900

    M1task2

---
+++ Permenye.py
@@ -0,0 +1,8 @@
+made_home_task = 12
+print (made_home_task)
+hours_spent = 1.5
+print(hours_spent)
+course_name = "python"
+print (hours_spent /made_home_task)
+time_one_task=(float(hours_spent/made_home_task))
+print(made_home_task,hours_spent,course_name,time_one_task)
