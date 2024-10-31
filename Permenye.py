6483b3f248a53974d27551007c87c83e2f32ec2a
HooryG <hooryegor@gmail.com>
2024-10-31 19:22:37 +0900

    Offprint31/10 M1task2

--- Permenye.py
+++ Permenye.py
@@ -1,18 +1,18 @@
 course_name = "Курс: Python"
-print (course_name)
+#print (course_name)
 made_home_task = int(12)
-print(made_home_task)
+#print(made_home_task)
 name_made_home_task= "Колличество заданий"
-print (name_made_home_task)
-print(name_made_home_task,made_home_task)
+#print (name_made_home_task)
+#print(name_made_home_task,made_home_task)
 hours_spent =float(1.5)
 name_hours_spent = "Количество потраченых часов"
-print(name_hours_spent)
-print(name_hours_spent,hours_spent)
+#print(name_hours_spent)
+#print(name_hours_spent,hours_spent)
 time_one_task = (hours_spent/made_home_task)
-print(float(time_one_task))
+#print(float(time_one_task))
 name_time_one_task="Потраченное время на одно задание"
-print(time_one_task,name_time_one_task)
+#print(time_one_task,name_time_one_task)
 print(course_name,"/",name_made_home_task,made_home_task,"/",name_hours_spent,hours_spent,"/",name_time_one_task,time_one_task)
 #Еслиб я знал как обращаться к строке напряму сделал бы короче. По заданию я понял, что выводить нужно с текстом.
 #Могу убрать проверочные print.
