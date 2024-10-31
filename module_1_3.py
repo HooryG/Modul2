ca3a8e752f5032be31f1d1742b048fee74ea8612
HooryG <hooryegor@gmail.com>
2024-10-31 15:00:04 +0900

    31/10

--- module_1_3.py
+++ module_1_3.py
@@ -2,10 +2,10 @@ from operator import truediv
 
 name = ("Konstantin")
 print(name,type(name))
-
 age = (48)
 print(age,type(age))
-age = (48+2)
-print(age,type(age))
+newage = (2)
+print(age + newage)
+print(age+ newage,type(age+newage))
 name =("i_student")
 print(name,type(name),bool(name))
