d0815bcbfb2b0298ab6628fe94bacd5783d7f379
HooryG <hooryegor@gmail.com>
2024-11-01 16:18:15 +0900

    M1task2

---
+++ module_1_4.py
@@ -0,0 +1,14 @@
+my_string = input("Сколько символов в даной строке ".upper().replace("С","К"))
+namber_of_char = (len("Сколько символов в даной строке "))
+print("Верный ответ: ",namber_of_char)
+my_string = input("ВВЕДИТЕ ВЕРНЫЙ ОТВЕТ: ".lower())
+print(type (my_string))
+true_q = ("Вы правильно ответили: ".upper().replace(" ","" ))
+print(true_q,my_string)
+print(my_string[0])  # Первая вводимая цифра и являеться 1 символом
+print(my_string[-1]) # Вторая вводимая цифра и являеться последним символом
+name = "Вы молодец"
+print(name[0])
+print(name[-1])
+
+
