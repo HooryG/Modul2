33215d3a6f922f44ed449ef758d88060207e64e8
HooryG <hooryegor@gmail.com>
2024-11-06 20:19:14 +0900

    06/11/2024

--- ratingbook.py
+++ ratingbook.py
@@ -1,6 +1,5 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = ['Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron']
-print(students,type(students))
 reit_book={}
 reit_book[students[4]]=sum(grades[0])/len(grades[0])
 reit_book[students[1]]=sum(grades[1])/len(grades[1])
